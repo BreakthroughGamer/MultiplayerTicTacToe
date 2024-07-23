3cfedece0bf788c1bb5e8138a8999b878910c8c8
BreakthroughGamer <55695819+BreakthroughGamer@users.noreply.github.com>
2024-07-23 11:29:40 -0300

    removed unwanted files

---
+++ index.js
@@ -0,0 +1,6 @@
+// This is the Node. Consider it to be the server. This script is launched when the node launches. 
+// This script needs to contain end-points which apply the game logic to the client's game-state and the server's game-state.
+// Serverside game-state (autoratative game-state) can be managed here.
+
+const express = require('express')
+const app = express()
